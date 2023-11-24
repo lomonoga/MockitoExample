97f394c1fab5ca4fd70a9a19a3bdfa6fbdfd9e26
Ilya <trintsa@mail.ru>
2023-11-24 13:01:53 +0500

    Initial commit

---
+++ src/test/java/ShoppingServiceTest.java
@@ -0,0 +1,10 @@
+import customer.CustomerDao;
+import org.mockito.Mockito;
+
+/**
+ * Тестирование класса {@link ShoppingService}
+ */
+public class ShoppingServiceTest
+{
+    private final CustomerDao customerDaoMock = Mockito.mock(CustomerDao.class);
+}
